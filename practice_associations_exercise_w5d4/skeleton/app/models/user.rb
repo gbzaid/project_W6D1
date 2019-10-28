332312befed52668c0475f2762b9bc8500a997f3
Zaid Mansuri <gbzaid@gmail.com>
2019-10-28 17:56:49 -0400

    push

---
+++ practice_associations_exercise_w5d4/skeleton/app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+
+    has_many :enrollments,
+        class_name: 'Enrollment',
+        primary_key: :id,
+        foreign_key: :student_id
+
+    has_many :courses_taught,
+        class_name: 'Course',
+        primary_key: :id,
+        foreign_key: :professor_id
+
+    
+    has_many :enrolled_courses, 
+        through: :enrollments,
+        source: :course
+        
+end
